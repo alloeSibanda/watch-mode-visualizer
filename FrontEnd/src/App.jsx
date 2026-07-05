b614f5c2ca87baca7523800ecb5c70e9869b4f05
Alloe Sibanda <sibandaalloe1@gmail.com>
2026-07-04 22:08:45 -0600

    Add AWS environment

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthProvider } from './context/AuthContext'; // ◄ Verify this file was fully replaced with the Supabase code!
+import { AuthProvider } from './context/AuthContext'; // ◄ Verify this file was fully replaced with the Supabase code!!
 import WorkspaceContainer from './components/WorkspaceContainer';
 
 function App() {
